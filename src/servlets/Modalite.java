81f0befbe1aef9c473e085a8cc905c0c4a85bd7a
paulcottin <paulcottin@gmail.com>
2015-03-03 18:49:15 +0100

    servlets opérationnels pour modalité, présentation, Double diplomes, étudiant

---
+++ src/servlets/Modalite.java
@@ -0,0 +1,31 @@
+package servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import bdd.InfosModalite;
+
+public class Modalite extends HttpServlet{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	InfosModalite infoMod;
+	
+	public Modalite() {
+		infoMod = new InfosModalite();
+	}
+	
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setAttribute("mod", infoMod.getInfos());
+		
+		this.getServletContext().getRequestDispatcher( "/WEB-INF/login.jsp" ).forward( request, response );
+	}
+
+}
