dca75ed44c58d7af0de471a221038b9bc6745b82
paulcottin <paulcottin@gmail.com>
2015-03-03 16:39:53 +0100

    ajout du bean doubleDiplome

--- src/bdd/InfosDoubleDiplomes.java
+++ src/bdd/InfosDoubleDiplomes.java
@@ -1,11 +1,18 @@
 package bdd;
 
+import beans.DoubleDiplomeBean;
+
 public class InfosDoubleDiplomes {
 	
-	
+	DoubleDiplomeBean ddBean;
 	
 	public InfosDoubleDiplomes(){
+		ddBean = new DoubleDiplomeBean();
+	}
+	
+	public DoubleDiplomeBean getInfos(){
 		
+		return ddBean;
 	}
 
 }
