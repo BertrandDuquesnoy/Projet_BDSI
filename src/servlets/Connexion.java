3d6f889c2d6a783d33afb129f30dae9b27030ff9
paulcottin@gmail.com <paulcottin@gmail.com>
2015-03-18 21:53:17 +0100

    merge manuel

--- src/servlets/Connexion.java
+++ src/servlets/Connexion.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package servlets;
 
 import java.io.IOException;
@@ -28,36 +27,4 @@ public class Connexion extends HttpServlet{
 		
 		this.getServletContext().getRequestDispatcher( "/WEB-INF/connexion.jsp" ).forward( request, response );
 	}
-	}
-=======
-package servlets;
-
-import java.io.IOException;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import modele.LdapConnectMdp;
-import modele.LdapException;
-
-
-public class Connexion extends HttpServlet{
-
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	
-	
-	public Connexion(){
-	}
-	
-	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
-		
-		
-		this.getServletContext().getRequestDispatcher( "/WEB-INF/connexion.jsp" ).forward( request, response );
-	}
-	}
->>>>>>> b2dc76f3c194ebe353b0cf6a7c10780cb044015f
+}
