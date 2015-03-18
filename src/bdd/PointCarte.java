8a8606aa1837948ba22653f179f865ecc4983aa4
Lolita Boumelit <lolita.boumelit@telecomnancy.net>
2015-03-19 00:57:23 +0100

    PointCarte.java

---
+++ src/bdd/PointCarte.java
@@ -0,0 +1,62 @@
+package bdd;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import beans.map.PointCarteBean;
+
+public class PointCarte {
+
+	String pilote = "com.mysql.jdbc.Driver";
+	String nomBase = "bdsi_sie1";
+	PointCarteBean pcb;
+	
+	public PointCarte() {
+		pcb = new PointCarteBean();
+	}
+	
+	public PointCarteBean getInfos(){
+		//TODO: Requêtes pour récupérer toutes les universités, stages et ddiplomes avec les infos demandées dans les beans correspondants
+		int id_pcb=0;
+		try{
+			Class.forName(pilote);
+
+			Connection connexion = DriverManager.getConnection("jdbc:mysql://bdsi.telecomnancy.eu:3306/"+nomBase,"sie1","sie1");
+
+			Statement instruction = connexion.createStatement();
+
+			//on recupere le lien concerant les aides financieres pour partir etudier a l'etranger
+			ResultSet resultat = instruction.executeQuery("SELECT * " +"FROM universite u, lien l "+"WHERE l.etr_univ = u.id_univ AND id = "+id_pcb+";");
+			while(resultat.next()){
+				pcb.setTitre(resultat.getString("nom"));
+				pcb.setDescription(resultat.getString("description"));
+				pcb.setLien(resultat.getString("l_web"));
+				pcb.setAdresse(resultat.getString("adresse"));
+				pcb.setType(resultat.getString("type"));
+			}
+			
+			ResultSet resultat2 = instruction.executeQuery("SELECT * " + "FROM entreprise ent, lien l "+"WHERE l.etr_entreprise = ent.id_entreprise AND id = "+id_pcb+";");
+			while(resultat.next()){
+				pcb.setTitre(resultat.getString("nom"));
+				pcb.setDescription(resultat.getString("domaine"));
+				pcb.setLien(resultat.getString("l_web"));
+				pcb.setAdresse(resultat.getString("adresse"));
+				pcb.setType(resultat.getString("type"));
+			}
+			
+		}
+		catch (Exception e){
+
+			System.out.println("echec pilote : "+e);
+		}
+
+		
+		return pcb;
+	}
+	
+
+	
+}
+
