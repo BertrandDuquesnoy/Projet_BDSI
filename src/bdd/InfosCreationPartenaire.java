b761a6e81cec0a4177f533cdd2e8211243cd4294
Bertrand Duquesnoy <bertrand.duquesnoy@telecomnancy.net>
2015-03-19 00:57:43 +0100

    creationpartenaire bdd, beans, servlet en cours

--- src/bdd/InfosCreationPartenaire.java
+++ src/bdd/InfosCreationPartenaire.java
@@ -18,8 +18,25 @@ public class InfosCreationPartenaire{
 	public InfosCreationPartenaire(){
 		newP = new CreationPartenaireBean();
 	}
-
-	public CreationPartenaireBean infoCreationPartenaire(){
+	String nom="null";
+	String pays="null";
+	String adresse="null";
+	String ville="null";
+	String logo_path="null";
+	String type="null";
+	String langue="null";
+	String date_creation="null";
+	String description="null";
+	String doub_dip="";
+	int note=0;
+	int nb_etudiant=0;
+	
+	public CreationPartenaireBean infoCreationPartenaire(String nom,String pays,String adresse,String ville,String logo_path,String type,
+			String langue,String date_creation,String description,String doub_dip,int nb_etudiant){
+		
+		nom=nom; pays=pays; adresse=adresse; ville=ville; logo_path=logo_path; type=type; langue=langue; date_creation=date_creation;
+		description=description; doub_dip=doub_dip; nb_etudiant=nb_etudiant;
+		
 		try{
 			Class.forName(pilote);
 
@@ -27,18 +44,8 @@ public class InfosCreationPartenaire{
 
 			Statement instruction = connexion.createStatement();
 
-			ResultSet resultat = instruction.executeQuery("SELECT * " +"FROM profil "+ ";");
-			
-			while(resultat.next()){
-			
-			}
-			
-			//On ne peut pas avoir la coloration d'un étudiant avec LDAP :(
-			
-			
-			
-			
-			
+			instruction.executeUpdate("INSERT INTO universite "+"VALUES ('', "+nom+", "+pays+", "+adresse+", "+ville+", "+logo_path+", "+type+
+					", "+langue+", "+date_creation+", 0, "+nb_etudiant+", "+description+", "+doub_dip+")");
 		}
 		catch (Exception e){
 
