3842f8554ae4fc8deb400efdea5ab67d7390d00a
paulcottin@gmail.com <paulcottin@gmail.com>
2015-03-19 08:25:42 +0100

    page contact fonctionnelle

--- src/servlets/Contact.java
+++ src/servlets/Contact.java
@@ -18,12 +18,10 @@ public class Contact extends HttpServlet{
 	 */
 	private static final long serialVersionUID = 1L;
 
-	InfosPersonne infos_pers;
-	InfosUniversity infos_univ;
+	
 
 	public Contact(){
-		infos_pers = new InfosPersonne();
-		infos_univ = new InfosUniversity();
+	
 	}
 
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
@@ -41,8 +39,6 @@ public class Contact extends HttpServlet{
 			this.getServletContext().getRequestDispatcher( "/WEB-INF/contact.jsp" ).forward( request, response );
 		}
 
-
-		this.getServletContext().getRequestDispatcher( "/WEB-INF/contact.jsp" ).forward( request, response );
 	}
 
 }
